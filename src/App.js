9162e03f82b38478fa6730ab65d8cca9c54ca900
Matias <ma.antoniorodriguez@gmail.com>
2025-06-15 07:55:17 -0400

    Add styled-components for global styling and remove redundant GlobalStyle from index.js

--- src/App.js
+++ src/App.js
@@ -2,10 +2,21 @@ import React from "react";
 import Header from "./componentes/Header";
 import Projects from "./componentes/Projects";
 import Contact from "./componentes/Contact";
+import { createGlobalStyle } from "styled-components";
+
+const GlobalStyle = createGlobalStyle`
+  body {
+    margin: 0;
+    font-family: 'Segoe UI', sans-serif;
+    background-color: white;
+    color: #1f2937;
+  }
+`;
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Projects />
       <Contact />
--- src/index.js
+++ src/index.js
@@ -1,21 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import { createGlobalStyle } from "styled-components";
-
-const GlobalStyle = createGlobalStyle`
-  body {
-    margin: 0;
-    font-family: 'Segoe UI', sans-serif;
-    background-color: white;
-    color: #1f2937;
-  }
-`;
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <>
-    <GlobalStyle />
-    <App />
-  </>
-);
+root.render(<App />);
